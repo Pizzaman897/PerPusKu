8229ba71117d2e01ce0fc5a28016d8ef1986ebf1
josemarvinlau2009-rgb <josemarvinlau2009@gmail.com>
2026-09-09 12:02:06 +0700

    implement: login blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Halaman Login
+Route::name('admin.')->prefix('admin')->group(function() {
+
+Route::get('/login',[AdminController::class, 'login'])->name('login');
+
 });
